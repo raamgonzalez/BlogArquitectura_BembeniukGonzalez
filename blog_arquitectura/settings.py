f54d604cb9458c344f14830e332c4a9becabc9a4
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-02-04 19:04:53 -0300

    Deploy Vercel

--- blog_arquitectura/settings.py
+++ blog_arquitectura/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7zs1tzf$_qyda(a$pzjwys$51a)llaft6g0y&&_@gl%y(((pl2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
